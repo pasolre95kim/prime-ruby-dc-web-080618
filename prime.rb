51f8ef9fd4ffc4da588271929a8fbb331f8098d2
Michelle Kim <pasolre95kim@gmail.com>
2018-07-13 22:40:25 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
 # Add  code here!
 def prime?(n)
- while n <= 2
-     return false
+ if n < 2
+   return false
+  (2..Math.sqrt(n)).each do |i|
     if n % i == 0
-      (2..Math.sqrt(n)).each do |i|
   return false
-else
- true
-
+end
+   true
 end
