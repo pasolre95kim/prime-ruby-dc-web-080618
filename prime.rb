93df22f5918ddf61956c56682c97479fdf0340e0
Michelle Kim <pasolre95kim@gmail.com>
2018-07-13 22:44:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,7 @@
-# Add  code here!
 def prime?(n)
   return false if n < 2
-  (2..Math.sqrt(n)).each do |i|
-  return false if n % i == 0
-end
-   true
+  (2..Math.sqrt(n)).each do |num|
+    return false if n % num == 0
+  end
+  true
 end
